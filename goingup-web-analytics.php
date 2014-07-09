aae5ebf81cffb2c482b48119a950af2f5e3315d1
goingup <goingup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-09 07:52:02 +0000

    Improves wordpress integration and access to settings. Adds input for chat and support widgets.
    git-svn-id: https://plugins.svn.wordpress.org/goingup-web-analytics/trunk@945427 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- goingup-web-analytics.php
+++ goingup-web-analytics.php
@@ -4,107 +4,85 @@ Plugin Name: GoingUp! Web Analytics
 Plugin URI: http://www.goingup.com/
 Description: GoingUp! Web Analytics is an advanced website traffic, SEO, and visitor analytics application which offers comprehensive visitor activity as well as search engine optimization and site ranking. Free to use, start GoingUp! today!.
 Author: GoingUP!
-Version: 3.6
+Version: 3.9
 
 Requires WordPress 2.1 or later. Not for use with WPMU.
 */
 
 function gustats_getcontent($url,$user_agent=''){
-	$url_parsed = parse_url($url);
-    if ( empty($url_parsed['scheme']) ) {
-        $url_parsed = parse_url('http://'.$url);
-    }
-    $rtn['url'] = $url_parsed;
 
-    $port = $url_parsed["port"];
-    if ( !$port ) {
-        $port = 80;
-    }
-    $rtn['url']['port'] = $port;
-    
-    $path = $url_parsed["path"];
-    if ( empty($path) ) {
-            $path="/";
-    }
-    if ( !empty($url_parsed["query"]) ) {
-        $path .= "?".$url_parsed["query"];
-    }
-    $rtn['url']['path'] = $path;
+	$tries = 0;
+	$maxTries = 5;
+	$repeat = true;
+	do {
+		$tries++;
+		if ($tries >= $maxTries) {
+			$repeat = false;
+			break;
+		}
 
-    $host = $url_parsed["host"];
-    $foundBody = false;
+		$rtn = array(
+			'http_code' => null,
+			'http_hreader' => null,
+			'body' => null
+		);
 
-   
-    
-    $out = "GET $path HTTP/1.0\r\n";
-    $out .= "Host: $host\r\n";
-    if ($user_agent){
-    	$out .= "user-agent: ".$user_agent;
-    }
-    $out .= "Connection: Close\r\n\r\n";
-
-    if ( !$fp = @fsockopen($host, $port, $errno, $errstr, 30) ) {
-        $rtn['errornumber'] = $errno;
-        $rtn['errorstring'] = $errstr;
-        return $rtn;
-    }
-    fwrite($fp, $out);
-    while (!feof($fp)) {
-        $s = fgets($fp, 128);
-        if ( $s == "\r\n" ) {
-            $foundBody = true;
-            continue;
-        }
-        if ( $foundBody ) {
-            $body .= $s;
-        } else {
-            if ( ($followRedirects) && (stristr($s, "location:") != false) ) {
-                $redirect = preg_replace("/location:/i", "", $s);
-                return HttpGet( trim($redirect) );
-            }
-            $header .= $s;
-        }
-    }
-    fclose($fp);
-
-    $__ = explode("\n",$header);
-    $___ = explode(" ",$__[0]);
-    
-    $rtn['header'] = ($header);
-    $rtn['http_code'] = trim($___[1]);
-    $rtn['http_header'] = trim($__[0]);
-    $rtn['body'] = trim($body);
-    return $rtn;
-}
+		// Create a stream
+		$opts = array(
+			'http'=>array(
+				'method'=>"GET",
+				'header'=>"Cache-Control:no-cache,no-store\r\n".
+				          "Connection:close\r\n"
+			)
+		);
+		$context = stream_context_create($opts);
+		// Open the file using the HTTP headers set above
+		$file = file_get_contents($url, false, $context);
+		if($http_response_header) {
+			foreach ($http_response_header as $header) {
+				if ($header == "HTTP/1.1 200 OK") {
+					$repeat = false;
+					$rtn['http_code'] = 200;
+					$rtn['body'] = $file;
+				}
+			}
+		}
+	} while ($repeat);
+    return $rtn;}
 
 
 function gustats_set(){
 	global $plugin_page;
 	global $api_key;
-	$api_key = get_option('gstats_api_key');
 	global $ws_key;
-	$ws_key = get_option('gstats_ws_key');
+	global $widget_codes;
 	?>
 	<div class="wrap">
 	<?php
 	if(!empty($_POST[api_key]) AND !empty($_POST[ws_key])){
-		$res = gustats_getcontent("http://www.goingup.com/xml/serialized.php?api=".$_POST['api_key']."&ws=".$_POST['ws_key']);
+		$res = gustats_getcontent("http://www.goingup.com/xml/serialized.php?api=".$_POST['api_key']."&ws=".$_POST['ws_key']."&rand=".mt_rand());
 		if ($res['http_code']!='200'){
 			echo "<center><span style='color:red;'><strong>" . __("There is communication problem with www.goingup.com") . "</strong></span><br/></center>";
-		}else{			
+		}else{
 			$fcont = $res['body'];
 			$web_details = unserialize($fcont);
-			
+
 			if($web_details){
 				update_option('gstats_api_key', $_POST[api_key]);
 				update_option('gstats_ws_key', $_POST[ws_key]);
+				update_option('gstats_widget_codes', $_POST[ws_widget_codes]);
 				$api_key = $_POST[api_key];
 				$ws_key = $_POST[ws_key];
-				echo "<center><span style='color:blue;'><strong>" . __("API key and Site Id are saved!") . "</strong></span><br/></center>";
+				$widget_codes = stripslashes($_POST[ws_widget_codes]);
+				echo "<center><span style='color:blue;'><strong>" . __("API Key, Site ID and Widget Codes are saved!") . "</strong></span><br/></center>";
 			}else{
-				echo "<center><span style='color:red;'><strong>" . __("Wrong API key or Site Id, please check it!") . "</strong></span><br/></center>";
+				echo "<center><span style='color:red;'><strong>" . __("Wrong API Key or Site ID, please check it!") . "</strong></span><br/></center>";
 			}
 		}
+	} else {
+    	$api_key = get_option('gstats_api_key');
+    	$ws_key = get_option('gstats_ws_key');
+    	$widget_codes = stripslashes(get_option('gstats_widget_codes'));
 	}
 	?>
 		<h2><?php _e('GoingUP! Web Analytics'); ?></h2>
@@ -112,7 +90,8 @@ function gustats_set(){
 			<form action="options-general.php?page=<?php echo $plugin_page; ?>" method="post">
 				<p><?php _e('Not GoingUp? <a href="http://www.goingup.com/signup.html" target="_blank"> Click here</a> to create a free account'); ?></p>
 				<label for="api_key"><?php _e('API Key:'); ?> <input style='width:285px' type="text" name="api_key" id="api_key" value="<?php echo $api_key; ?>" /></label>
-				<label for="ws_key"><?php _e('Site ID:'); ?> <input style='width:95px' type="text" name="ws_key" id="ws_key" value="<?php echo $ws_key; ?>" /></label>
+				<label for="ws_key"><?php _e('Site ID:'); ?> <input style='width:95px' type="text" name="ws_key" id="ws_key" value="<?php echo $ws_key; ?>" /></label></br>
+				<label for="ws_widget_codes"><?php _e('Widget Codes:'); ?></br><textarea style='width:481px' rows='10' name="ws_widget_codes" id="ws_widget_codes"><?php echo $widget_codes; ?></textarea></label>
 				<p class="submit"><input type="submit" value="<?php _e('Save &raquo;'); ?>" /></p>
 			</form>
 		</div>
@@ -124,13 +103,15 @@ function gustats_admin_menu(){
 	add_submenu_page('options-general.php', __('GoingUp! Web Analytics'), __('GoingUp! Web Analytics'), 'manage_options', 'gustats', 'gustats_set');
 	add_option('gstats_api_key', '', 'GoingUp! stats api key');
 	add_option('gstats_ws_key', '', 'GoingUp! stats ws key');
+	add_option('gstats_widget_codes', '', 'GoingUp! widget codes');
 }
 
 function gustat_footer(){
 	$workdir = get_option('siteurl')."/".PLUGINDIR."/goingup-web-analytics/";
-	$api_key = get_option('gstats_api_key');	
+	$api_key = get_option('gstats_api_key');
 	$ws_key = get_option('gstats_ws_key');
-	
+	$widget_codes = stripslashes(get_option('gstats_widget_codes'));
+
 	if (isset($ws_key) and isset($api_key))	{
 		$gu_track_host = urlencode("http://".$_SERVER['HTTP_HOST']);
 		$gu_track_referrer = urlencode($_SERVER['HTTP_REFERER']);
@@ -138,11 +119,31 @@ function gustat_footer(){
 		$gu_track_agent = urlencode($_SERVER['HTTP_USER_AGENT']);
 		$gu_track_uri = urlencode($_SERVER['REQUEST_URI']);
 		$gu_track_websiteid = $ws_key;
-		$gu_track_urlparams = "st=".$gu_track_websiteid."&vip=".$gu_track_ipadress."&cur=".$gu_track_host.$gu_track_uri."&ref=".$gu_track_referrer."&ua=".$gu_track_agent."&b=5";
+		$gu_track_urlparams = "st=".$gu_track_websiteid."&vip=".$gu_track_ipadress."&cur=".$gu_track_host.$gu_track_uri."&ref=".$gu_track_referrer."&ua=".$gu_track_agent."&b=5&rand=".mt_rand();
 		$res = gustats_getcontent("http://counter.goingup.com/phptrack.php?".$gu_track_urlparams);
 		echo "<div style=\"display:none\">".$res['body']."</div>";
-	
+
 	}
 }
+
+function gustat_head(){
+    $api_key = get_option('gstats_api_key');
+    $ws_key = get_option('gstats_ws_key');
+    $widget_codes = stripslashes(get_option('gstats_widget_codes'));
+
+    if (isset($ws_key) and isset($api_key))	{
+        echo $widget_codes;
+
+    }
+}
+/* settings link in plugin management screen */
+function gustats_admin_menu_link($actions, $file) {
+	//if(false !== strpos($file, 'gustats')) {
+		$actions['settings'] = '<a href="options-general.php?page=gustats">Settings</a>';
+	//}
+	return $actions; 
+}
+add_filter('plugin_action_links', 'gustats_admin_menu_link', 2, 2);
 add_action('admin_menu', 'gustats_admin_menu');
 add_action('wp_footer', 'gustat_footer');
+add_action('wp_head', 'gustat_head');
